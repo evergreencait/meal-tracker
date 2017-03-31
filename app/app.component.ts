e58ce3587b84d01c8a8bb72cc8a218e8116af0c7
Caitlin Hines <caitlinhines@me.com>
2017-03-31 08:38:27 -0700

    include export class in root component directly and it works

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,6 +6,9 @@ import { Component } from '@angular/core';
   <div class="container">
     <h1>Meal Tracker for {{month}}/{{day}}/{{year}}</h1>
     <h3>{{currentFocus}}</h3>
+    <ul>
+     <li>{{firstMeal.name}}</li>
+   </ul>
   </div>
   `
 })
@@ -16,4 +19,10 @@ export class AppComponent {
   month: number = this.currentTime.getMonth() + 1;
   day: number = this.currentTime.getDate();
   year: number = this.currentTime.getFullYear();
+
+  firstMeal: Meal = new Meal("Mac and cheese", "only ate half of it", 400);
+}
+
+export class Meal {
+  constructor(public name: string, public details: string, public calories: number) { }
 }
